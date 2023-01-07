18533a1dacdae49bc72b06fc33babd851ed102f9
deep1401 <gandhi0869@gmail.com>
2023-01-07 15:29:13 -0700

    Add recommend_sys code and required files in data/

---
+++ recommend_sys.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import nltk
+import string
+import ast
+import re
+import unidecode
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+import unidecode
+import ast
+from collections import Counter
+nltk.download('wordnet')
+
+
+def ingredient_parser(ingreds):
+    measures = ['teaspoon', 't', 'tsp.', 'tsp', 'tablespoon', 'T', 'tbl.', 'tb', 'tbsp.', 'tbsp', 'fluid ounce', 'fl oz', 'gill', 'cup', 'c', 'pint', 'p', 'pt', 'fl pt', 'quart', 'q', 'qt', 'fl qt', 'gallon', 'g', 'gal', 'ml', 'milliliter', 'millilitre', 'cc', 'mL', 'l', 'liter', 'litre', 'L', 'dl', 'deciliter', 'decilitre', 'dL', 'bulb', 'level',
+                'heaped', 'rounded', 'whole', 'pinch', 'medium', 'slice', 'pound', 'lb', '#', 'ounce', 'oz', 'mg', 'milligram', 'milligramme', 'g', 'gram', 'gramme', 'kg', 'kilogram', 'kilogramme', 'x', 'of', 'mm', 'millimetre', 'millimeter', 'cm', 'centimeter', 'centimetre', 'm', 'meter', 'metre', 'inch', 'in', 'milli', 'centi', 'deci', 'hecto', 'kilo']
+    words_to_remove = ['depending', 'get', 'about', 'sturdy', 'fresh', 'oil', 'a', 'red', 'bunch', 'and', 'clove', 'or', 'leaf', 'chilli', 'large', 'extra', 'sprig', 'ground', 'handful', 'free', 'small', 'pepper', 'virgin', 'range', 'from', 'dried', 'sustainable', 'black', 'peeled', 'higher', 'welfare', 'seed', 'for', 'finely', 'freshly', 'sea', 'quality', 'white', 'ripe', 'few', 'piece', 'source', 'to', 'organic', 'flat', 'smoked', 'ginger', 'sliced', 'green', 'picked', 'the', 'stick', 'plain', 'plus', 'mixed', 'mint', 'bay', 'basil', 'your', 'cumin', 'optional', 'fennel', 'serve', 'mustard', 'unsalted', 'baby', 'paprika', 'fat', 'ask', 'natural', 'skin', 'roughly', 'into', 'such', 'cut', 'good', 'brown', 'grated', 'trimmed', 'oregano', 'powder', 'yellow', 'dusting', 'knob', 'frozen', 'on', 'deseeded', 'low', 'runny', 'balsamic', 'cooked', 'streaky', 'nutmeg', 'sage', 'rasher', 'zest', 'pin', 'groundnut', 'breadcrumb', 'turmeric', 'halved', 'grating', 'stalk', 'light', 'tinned', 'dry', 'soft', 'rocket', 'bone', 'colour', 'washed', 'skinless', 'leftover', 'splash', 'removed', 'dijon', 'thick', 'big', 'hot', 'drained', 'sized', 'chestnut', 'watercress', 'fishmonger', 'english', 'dill', 'caper', 'raw', 'worcestershire', 'flake', 'cider', 'cayenne', 'tbsp', 'leg', 'pine', 'wild', 'if', 'fine', 'herb', 'almond', 'shoulder', 'cube', 'dressing', 'with', 'chunk', 'spice', 'thumb', 'garam', 'new', 'little', 'punnet', 'peppercorn', 'shelled',
+                       'saffron', 'other', 'chopped', 'salt', 'olive', 'taste', 'can', 'sauce', 'water', 'diced', 'package', 'italian', 'shredded', 'divided', 'parsley', 'vinegar', 'all', 'purpose', 'crushed', 'juice', 'more', 'coriander', 'bell', 'needed', 'thinly', 'boneless', 'half', 'thyme', 'cubed', 'cinnamon', 'cilantro', 'jar', 'seasoning', 'rosemary', 'extract', 'sweet', 'baking', 'beaten', 'heavy', 'seeded', 'tin', 'vanilla', 'uncooked', 'crumb', 'style', 'thin', 'nut', 'coarsely', 'spring', 'chili', 'cornstarch', 'strip', 'cardamom', 'rinsed', 'honey', 'cherry', 'root', 'quartered', 'head', 'softened', 'container', 'crumbled', 'frying', 'lean', 'cooking', 'roasted', 'warm', 'whipping', 'thawed', 'corn', 'pitted', 'sun', 'kosher', 'bite', 'toasted', 'lasagna', 'split', 'melted', 'degree', 'lengthwise', 'romano', 'packed', 'pod', 'anchovy', 'rom', 'prepared', 'juiced', 'fluid', 'floret', 'room', 'active', 'seasoned', 'mix', 'deveined', 'lightly', 'anise', 'thai', 'size', 'unsweetened', 'torn', 'wedge', 'sour', 'basmati', 'marinara', 'dark', 'temperature', 'garnish', 'bouillon', 'loaf', 'shell', 'reggiano', 'canola', 'parmigiano', 'round', 'canned', 'ghee', 'crust', 'long', 'broken', 'ketchup', 'bulk', 'cleaned', 'condensed', 'sherry', 'provolone', 'cold', 'soda', 'cottage', 'spray', 'tamarind', 'pecorino', 'shortening', 'part', 'bottle', 'sodium', 'cocoa', 'grain', 'french', 'roast', 'stem', 'link', 'firm', 'asafoetida', 'mild', 'dash', 'boiling']
+    if isinstance(ingreds, list):
+        ingredients = ingreds
+    else:
+        ingredients = ast.literal_eval(ingreds)
+
+    translator = str.maketrans('', '', string.punctuation)
+    lemmatizer = WordNetLemmatizer()
+    ingred_list = []
+    for i in ingredients:
+        i.translate(translator)
+        items = re.split(' |-', i)
+        items = [word for word in items if word.isalpha()]
+        items = [word.lower() for word in items]
+        items = [unidecode.unidecode(word) for word in items]
+        items = [lemmatizer.lemmatize(word) for word in items]
+        items = [word for word in items if word not in measures]
+        items = [word for word in items if word not in words_to_remove]
+        if items:
+            ingred_list.append(' '.join(items))
+    ingred_list = " ".join(ingred_list)
+    return ingred_list
+
+
+def get_recommendations(N, scores):
+    df_recipes = pd.read_csv('./data/new_df.csv')
+    top = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:N]
+    recommendation = pd.DataFrame(
+        columns=['recipe_name', 'recipe', 'ingredients', 'score', 'image'])
+    count = 0
+    for i in top:
+        recommendation.at[count, 'recipe_name'] = df_recipes['Title'][i]
+        recommendation.at[count, 'recipe'] = df_recipes['Instructions'][i]
+        recommendation.at[count, 'ingredients'] = ingredient_parser_final(
+            df_recipes['Cleaned_Ingredients'][i])
+        recommendation.at[count, 'image'] = df_recipes['Image_Name'][i]
+        recommendation.at[count, 'score'] = "{:.3f}".format(float(scores[i]))
+        count += 1
+    return recommendation
+
+
+def ingredient_parser_final(ingredient):
+    if isinstance(ingredient, list):
+        ingredients = ingredient
+    else:
+        ingredients = ast.literal_eval(ingredient)
+
+    ingredients = ','.join(ingredients)
+    ingredients = unidecode.unidecode(ingredients)
+    return ingredients
+
+
+def rec_sys(ingredients, N=5):
+
+    # load in tdidf model and encodings
+    with open('./data/tfidf_encodings.pkl', 'rb') as f:
+        tfidf_encodings = pickle.load(f)
+
+    with open('./data/tfidf.pkl', "rb") as f:
+        tfidf = pickle.load(f)
+
+    # parse the ingredients using my ingredient_parser
+    try:
+        ingredients_parsed = ingredient_parser(ingredients)
+    except:
+        ingredients_parsed = ingredient_parser([ingredients])
+
+    # use our pretrained tfidf model to encode our input ingredients
+    ingredients_tfidf = tfidf.transform([ingredients_parsed])
+
+    # calculate cosine similarity between actual recipe ingreds and test ingreds
+    cos_sim = map(lambda x: cosine_similarity(
+        ingredients_tfidf, x), tfidf_encodings)
+    scores = list(cos_sim)
+
+    # Filter top N recommendations
+    recommendations = get_recommendations(N, scores)
+    return recommendations
+
+
+def get_all_ingredients():
+    return list(pd.read_json('./data/ingredients.json')['ingredients'].values)
